8bdf0360a56388375a535a34839b1a2e1026dc20
scharapovmichael <scharapovm@yandex.ru>
2022-06-29 21:38:40 +0700

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,32 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public static void main(String[] args){
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+    private String name;
+    private String food;
+
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
