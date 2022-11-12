1d1c87f3d5e05832776bfec7775135439bb0f34c
scharapovmichael <scharapovm@yandex.ru>
2022-11-12 18:57:57 +0700

    1.1. Создание объекта [#165452 #314978]

--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -4,6 +4,6 @@ public class Dog {
     public static void main(String[] args) {
         Dog polkan = new Dog();
         Dog sharik = new Dog();
-        Dog zhychka = new Dog();
+        Dog zhychka = new Dog()
     }
 }
