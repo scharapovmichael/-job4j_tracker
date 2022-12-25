e04581cb5bafa545185a7054b18fb2ccc0baeaa7
scharapovmichael <scharapovm@yandex.ru>
2022-12-25 17:30:56 +0700

    1.7. Конструктор [#173889 #321038]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -5,11 +5,11 @@ public class Error {
     private int status;
     private String message;
 
-public Error() {
+    public Error() {
 
-}
+    }
 
-public Error(boolean active, int status, String message) {
+    public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
