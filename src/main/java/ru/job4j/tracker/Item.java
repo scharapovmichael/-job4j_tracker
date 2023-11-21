a6121a59e5accf7ab99b02ac6fb274ec30582c27
scharapovmichael <scharapovm@yandex.ru>
2023-11-21 22:17:14 +0700

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,32 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name){
+        this.name = name;
+    }
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println();
+        System.out.println("айди" + id);
+        System.out.println("Имя" + name);
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.printInfo();
+        Item second = new Item("Паша");
+        second.printInfo();
+        Item third = new Item(10, "Степа");
+        third.printInfo();
+    }
+
     public int getId() {
         return id;
     }
